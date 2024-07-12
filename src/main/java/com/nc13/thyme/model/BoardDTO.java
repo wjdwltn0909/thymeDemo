8a0f215e4906950a8435f0ee6bec5e91475deed6
Jisue Jung <wjdwltn0909@gmail.com>
2024-07-12 11:20:57 +0900

    crete BoardMapper.xml and BoardDTO.java

---
+++ src/main/java/com/nc13/thyme/model/BoardDTO.java
@@ -0,0 +1,13 @@
+package com.nc13.thyme.model;
+
+import java.util.Date;
+
+public class BoardDTO {
+    private int id;
+    private String title;
+    private String content;
+    private int writerId;
+    private String nickname;
+    private Date entryDate;
+    private Date modifyDate;
+}
