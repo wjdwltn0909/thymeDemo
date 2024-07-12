cceab007794cb8cc9cc15f054c721331eb8d3e38
Jisue Jung <wjdwltn0909@gmail.com>
2024-07-12 10:18:40 +0900

    first commit

---
+++ src/main/java/com/nc13/thyme/ThymeApplication.java
@@ -0,0 +1,13 @@
+package com.nc13.thyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nc13/thyme/ThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc13.thyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
