cfe15bc5a47fa42e23d961ec28e7c0b6d50d12d6
Jisue Jung <wjdwltn0909@gmail.com>
2024-07-12 11:14:11 +0900

    MyBatisConfig.java & mybatis-config.xml finish

---
+++ src/main/java/com/nc13/thyme/config/MyBatisConfig.java
@@ -0,0 +1,33 @@
+package com.nc13.thyme.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyBatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfigLocation(
+                new ClassPathResource("mybatis/mybatis-config.xml")
+        );
+        Resource[] resources =
+                new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mappers/*.xml");
+        sqlSessionFactoryBean.setMapperLocations(resources);
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
