7e7f4e25592241f1e5ce82e63c20064a59190d45
Jisue Jung <wjdwltn0909@gmail.com>
2024-07-12 11:49:13 +0900

    add BoardService.java & HomeController.java & index.html

---
+++ src/main/java/com/nc13/thyme/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.nc13.thyme.service;
+
+import com.nc13.thyme.model.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final SqlSession SESSION;
+    private final String NAMESPACE = "mappers.BoardMapper";
+    
+    public List<BoardDTO> selectAll() {
+        return SESSION.selectList(NAMESPACE + ".selectAll");
+    }
+}
